0bc1166d93a8ad66b71a949d6b67a4c72d8b1021
jaimebe4 <jaimebe@gmail.com>
2023-08-14 22:54:25 -0500

    Se agrega Microcharts, Page Reports

---
+++ TallerApp/TallerApp/Views/ReportsPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microcharts;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TallerApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ReportsPage : ContentPage
+	{
+		private readonly ChartEntry[] entries = new[]
+		{
+			new ChartEntry(112)
+			{
+				Label = "UWP",
+				ValueLabel = "112",
+				Color = SKColor.Parse("#2c3e50")
+			},
+            new ChartEntry(648)
+            {
+                Label = "Android",
+                ValueLabel = "648",
+                Color = SKColor.Parse("#77d065")
+            },
+            new ChartEntry(428)
+            {
+                Label = "iOS",
+                ValueLabel = "428",
+                Color = SKColor.Parse("#b455b6")
+            },
+        };
+
+
+        public ReportsPage ()
+		{
+			InitializeComponent ();
+			charViewBar.Chart = new BarChart { Entries = entries, ValueLabelOrientation = Orientation.Horizontal, LabelTextSize = 30, AnimationProgress = 10, LabelOrientation = Orientation.Horizontal};
+            charViewPie.Chart = new PieChart { Entries = entries, LabelTextSize = 30, AnimationProgress = 10, HoleRadius = 0.3f };
+            charViewLine.Chart = new LineChart { Entries = entries, ValueLabelOrientation = Orientation.Horizontal, LabelTextSize = 30, AnimationProgress = 10, LabelOrientation = Orientation.Horizontal, LineMode = LineMode.Straight  };
+        }
+	}
+}
